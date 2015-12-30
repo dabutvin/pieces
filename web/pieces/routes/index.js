880c551fe64cb873ff2dd8ddd19d24367e4d9e38
dabutvin <dabutvin@microsoft.com>
2015-12-29 22:47:01 -0800

    add dummy data to a json route

--- web/pieces/routes/index.js
+++ web/pieces/routes/index.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/api/data', function(req, res, next) {
+	res.json({
+		pieces: [
+			{
+				title: "title uno",
+				artist: "artist uno",
+				medium: "medium uno",
+				src: "http://lorempixel.com/250/250/"
+			},
+			{
+				title: "title uno",
+				artist: "artist uno",
+				medium: "medium uno",
+				src: "http://lorempixel.com/250/250/"
+			}
+		] 
+	});
+});
+
 module.exports = router;
