4e598e6284f4032008fa814a358016ffae91096b
dabutvin <dabutvin@microsoft.com>
2015-12-30 00:40:40 -0800

    stub out piece detail activity

--- app/src/main/java/com/dabutvin/pieces/MainActivity.java
+++ app/src/main/java/com/dabutvin/pieces/MainActivity.java
@@ -1,6 +1,7 @@
 package com.dabutvin.pieces;
 
 import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -103,7 +104,8 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onItemClicked(int i, Object o) {
-
+                Intent intent = new Intent(getApplicationContext(), PieceDetailActivity.class);
+                startActivity(intent);
             }
         });
 
